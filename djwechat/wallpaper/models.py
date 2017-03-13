31649beba66ef9ff2e6dff4fc906a22ed2b929cb
仲夏叶 <stornado@qq.com>
2017-03-13 22:40:58 +0800

    增加翻译

--- djwechat/wallpaper/models.py
+++ djwechat/wallpaper/models.py
@@ -2,17 +2,34 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
 
+@python_2_unicode_compatible
 class Tag(models.Model):
     name = models.CharField(verbose_name=_('Name'), max_length=25)
 
+    def __str__(self):
+        return self.name
 
+    class Meta:
+        verbose_name = _('Tag')
+        verbose_name_plural = _('Tags')
+
+
+@python_2_unicode_compatible
 class Image(models.Model):
     title = models.CharField(verbose_name=_('title'), max_length=25)
     url = models.URLField(verbose_name=_('URL'))
     tags = models.ManyToManyField(Tag, verbose_name=_('Tags'))
     publishedAt = models.DateField(verbose_name=_('Published Date'))
     uuid = models.CharField(verbose_name=_('UUID'), max_length=32)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _('Image')
+        verbose_name_plural = _('Images')
