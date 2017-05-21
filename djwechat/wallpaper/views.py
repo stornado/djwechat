b119a3d2d1daa0e8d87956404f5d982d1301ddea
仲夏叶 <stornado@qq.com>
2017-05-21 18:02:30 +0800

    加入按名称搜索功能

--- djwechat/wallpaper/views.py
+++ djwechat/wallpaper/views.py
@@ -17,7 +17,15 @@ def home(request):
 
 
 def next(request):
-    all_images = get_list_or_404(Image, show=True)
+    return search(request)
+
+
+def search(request):
+    keyword = request.POST.get('keyword', '')
+    if not keyword:
+        all_images = get_list_or_404(Image, show=True)
+    else:
+        all_images = get_list_or_404(Image, show=True, title__icontains=keyword)
     try:
         limit = request.GET.get('limit', request.POST.get('limit', 5))
         limit = int(limit)
@@ -34,7 +42,3 @@ def next(request):
         images = paginator.page(paginator.num_pages)
     finally:
         return render(request, 'wallpaper/next_page.json', {'images': images}, content_type='application/json; charset=UTF-8')
-
-
-def search(request):
-    return JsonResponse({})
